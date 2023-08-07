9754624597269197073dd6679f38729eeb47cf7f
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-08-07 15:09:25 +0200

    Profile Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
+import Profile from "./PortfolioContainer/Home/Profile/Profile";
+import Home from "./PortfolioContainer/Home/Home";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home>
+        <Profile />
+      </Home>
+    </div>
+  );
 }
 
 export default App;
