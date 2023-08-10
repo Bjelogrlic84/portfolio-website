d20e71072681632847e37ff0110bce3112e03750
Bjelogrlic84 <bjelogrlic.ivan.84@gmail.com>
2023-08-10 18:08:47 +0200

    Fix

--- src/PortfolioContainer/Home/Footer/Footer.js
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -1,14 +1,12 @@
 import React from "react";
+import ShapeBg from "../../../assets/Home/shape-bg.svg";
 import "./Footer.css";
 
-export default function footer() {
+export default function Footer() {
   return (
     <div className="footer-container">
       <div className="footer-parent">
-        <img
-          src={require("../../../assets/Home/shape-bg.svg").default}
-          alt="you have problem with the image"
-        />
+        <img src={ShapeBg} alt="you have problem with the image" />
       </div>
     </div>
   );
